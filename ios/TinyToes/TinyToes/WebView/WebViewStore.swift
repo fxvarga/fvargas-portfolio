f7684b6eaf719cdbd768eb230f47cc23d3f63dad
fxvarga <fxvarga@gmail.com>
2026-05-01 21:22:06 -0400

    Fix Swift concurrency: mark WebViewStore as @MainActor

--- ios/TinyToes/TinyToes/WebView/WebViewStore.swift
+++ ios/TinyToes/TinyToes/WebView/WebViewStore.swift
@@ -2,6 +2,7 @@ import Foundation
 import WebKit
 
 /// Central store managing the WKWebView instance and its configuration.
+@MainActor
 class WebViewStore: ObservableObject {
   let webView: WKWebView
 
